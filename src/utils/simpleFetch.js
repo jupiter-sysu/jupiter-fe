424b997d68d96da3a29dc7a20eaf9f23e2e413ab
mcmichaelchan <623769018@qq.com>
2018-05-21 11:29:35 +0800

    feat(component): add simple fetch function

---
+++ src/utils/simpleFetch.js
@@ -0,0 +1,37 @@
+import { SERVER } from '../app-config/nework';
+
+function myFetch(url, method, data) {
+  const fullUrl = SERVER + url;
+  const data_string = JSON.stringify(data);
+  return new Promise((resolve) => {
+    if (method === 'GET') {
+      fetch(fullUrl, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      })
+        .then(res => res.json())
+        .then((res) => { resolve({ res }); })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else if (method === 'POST') {
+      fetch(fullUrl, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: data_string,
+      })
+        .then(res => res.json())
+        .then((res) => { resolve(res); })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  });
+}
+export default myFetch;
