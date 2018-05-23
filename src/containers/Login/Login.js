debf220455dedd34cb5fa6ddc37a0353c3f85f1d
chenhu <chenhu297@gmail.com>
2018-05-23 19:44:35 +0800

    fix(component): fix login problem and delete useless function in store/user.js

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
-import { View, StyleSheet, Text, ImageBackground, Dimensions, TextInput, StatusBar, NetInfo, Platform, Alert,  } from "react-native";
-import { observer, inject } from "mobx-react";
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, ImageBackground, Dimensions, TextInput, StatusBar, NetInfo, Platform, Alert, Keyboard } from 'react-native';
+import { observer, inject } from 'mobx-react';
 import { Button } from 'antd-mobile';
 import MCButton from '../../component/DataEntry/MCButton';
 import MCHeader from '../../component/Navigation/MCHeader';
 import { THEME_PRIMARY_COLOR } from './../../common-style/theme';
 import Form, { form } from './component/Login-form';
 import { Toast } from 'antd-mobile';
-import { myFetch } from '../../utils/fetch';
+import post from '../../utils/fetch';
 import MCSpinner from '../../component/Feedback/MCSpinner';
 import { NavigationActions } from 'react-navigation';
 
@@ -19,57 +19,67 @@ const PIXEL_RATE_Y = Dimensions.get('screen').height / 667;
 class Login extends Component {
     constructor(props) {
         super(props);
-        
-        form.$hooks.onSuccess = async(form) => {
-            let result = await this.props.user.loginWithPhone(form.$('phone').value, form.$('password').value);
-            if (result == 200) {
-                // this.props.navigation.navigate()
-                this.reset();
-                console.log("login success");
-            } else if (result == 401 || result == 402) {
-                console.log("Login fail");
-                setTimeout(() => {
-                    Toast.info("账号或密码错误，请检查您的输入");
-                }, 0);
-            }
-        }
-        
-        form.$hooks.onError = (form) => {
-            
-        }
+        this.reset = this.reset.bind(this);
 
-        
+        form.$hooks.onSuccess = async (form) => {
+            if (props.user.networkType === 'none' || props.user.networkType === 'NONE') {
+                Toast.info('暂无网络, 请检查您的网络设置', 2);
+            } else {
+                props.user.setModalVisible(true);
+                post('/login/mobile_login/', {
+                    phone_num: Number(form.$('phone').value),
+                    password: form.$('password').value,
+                }, (res) => {
+                    console.log(res);
+                    Keyboard.dismiss();
+                    props.user.login();
+                    props.user.setUserPhone(Number(form.$('phone').value));
+                    props.navigation.navigate('index');
+                }, (err) => {
+                    console.log(err);
+                    if (err.enmsg === 'unregistered_phone' || err.enmsg === 'wrong_password') {
+                        Toast.info('账号或密码错误, 请检查您的输入', 2);
+                    } else {
+                        Toast.info(err.cnmsg, 2);
+                    }
+
+                }, () => {
+                    this.props.user.setModalVisible(false);
+                }, );
+            }
+        };
+
+        form.$hooks.onError = (form) => {
+
+        };
+
         this.state = {
             networkType: true,
             modalVisible: false,
-        }
-    }
-    
-    FindPasswordPhone = () => {
-        this.props.navigation.navigate('FindPasswordPhone');
+        };
     }
 
     reset() {
         return this.props.navigation.dispatch(NavigationActions.reset({
             index: 0,
             actions: [
-                NavigationActions.navigate({ routeName: 'index' })
-            ]
+                NavigationActions.navigate({ routeName: 'index' }),
+            ],
         }));
     }
 
-    
     render() {
         return (
             <ImageBackground
                 style={styles.container}
                 source={require('./assets/White.png')}
             >
-                <StatusBar barStyle='dark-content' />
+                <StatusBar barStyle="dark-content" />
                 <MCSpinner isVisible={this.state.modalVisible} />
                 <MCHeader
                     handler={() => this.props.navigation.goBack()}
-                >手机号登录</MCHeader>
+                >手机号登录
+                </MCHeader>
 
                 <Form form={form} naviga={this.props.navigation} />
             </ImageBackground>
@@ -78,8 +88,8 @@ class Login extends Component {
 
     componentDidMount() {
         NetInfo.addEventListener('connectionChange', (networkType) => {
-            this.setState({networkType: networkType.type})
-        })
+            this.setState({ networkType: networkType.type });
+        });
     }
 }
 
@@ -98,5 +108,5 @@ const styles = StyleSheet.create({
         position: 'relative',
         left: 2 * PIXEL_RATE,
         top: 2 * PIXEL_RATE,
-    }
+    },
 });
--- src/store/user.js
+++ src/store/user.js
@@ -68,36 +68,6 @@ class user {
         }
     }
 
-    // Login state，直接登录
-    @action.bound
-    async loginWithPhone(phone = this.user.userphone, pass = this.user.userPassword) {
-        if (this.networkType === 'none' || this.networkType === 'NONE') {
-            return 0;
-        } else {
-            const postData = {
-                phone_num: Number(phone),
-                password: pass,
-            }
-            console.log(postData);
-            this.setModalVisible(true);
-
-            // 发送请求
-            let data = await myFetch('mobile_login/', 'POST', postData);
-            console.log(data);
-
-            this.setModalVisible(false);
-
-            if (data.error_code === 200) {
-                this.setUserPhone(Number(phone));
-                this.setUserPassword(pass);
-                this.isLogin = true;
-                return 200;
-            } else {
-                return data.error_code;
-            }
-        }
-    }
-
     // 找回密码时，请求验证码
     @action.bound
     async getFindPasswordConfirmCode(phone = this.user.userphone) {
@@ -121,61 +91,6 @@ class user {
         }
     }
 
-    // 找回密码时，提交验证码给服务器，判断验证码是否有效
-    @action.bound
-    async validateFindPasswordIDCode() {
-        if (this.networkType === 'none' || this.networkType === 'NONE') {
-            return 0;
-        } else {
-            const postData = {
-                mobile_message: this.confirmCode,
-            }
-
-            this.setModalVisible(true);
-
-            // 请求数据2
-            let data = await myFetch('verify_message', 'POST', postData);
-
-            this.setModalVisible(false);
-
-            console.log(data);
-
-            if (data.error_code === 412) {
-                return 412;
-            } else if (data.error_code === 200) {
-                console.log('200');
-                return 200;
-
-            }
-        }
-    }
-
-    // 找回密码时，提交新密码，更改密码
-    @action.bound
-    async setNewPassword(pass = this.user.userPassword) {
-        if (this.networkType === 'none' || this.networkType === 'NONE') {
-            return 0;
-        } else {
-            const postData = {
-                password: pass
-            }
-            console.log(postData);
-            this.setModalVisible(true);
-
-            // 请求数据2
-            let data = await myFetch('new_password', 'POST', postData);
-            console.log(data);
-
-            this.setModalVisible(false);
-
-            if (data.error_code === 401) {
-                return 401;
-            } else if (data.error_code === 200) {
-                this.setUserPassword(pass);
-                return 200;
-            }
-        }
-    }
     @persist @observable userphone = 0;
     @observable userPassword = '';
     @persist @observable isLogin = false;
