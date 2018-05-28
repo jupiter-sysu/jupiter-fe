736634ce7926882f5aad82fb51a34bd48e66ea20
chenhu <chenhu297@gmail.com>
2018-05-28 22:05:19 +0800

    feat(experience-detail-initial): 初步完成玩法页静态UI

--- src/containers/MainTab/Experience/containers/ExperienceDetail/tabContainers/Experience.js
+++ src/containers/MainTab/Experience/containers/ExperienceDetail/tabContainers/Experience.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Text, View, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, Dimensions, StyleSheet, TouchableOpacity, ImageBackground, Image, ScrollView } from 'react-native';
 import { inject, observer, } from 'mobx-react';
 import styled from 'styled-components';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import Item from '../../../component/Item'
 
 const PIXEL_RATE = Dimensions.get('screen').width / 375;
 const PIXEL_RATE_Y = Dimensions.get('screen').height / 667;
@@ -18,51 +19,145 @@ class Experience extends Component {
 
     }
 
+    rendTags() {
+        return this.props.experience.detail.data.tags.map((item) => {
+            return <Text style={styles.tags} key={item}>{item}</Text>
+        });
+    }
+
+    
+
     render() {
         console.log(this.props.navigation);
         return (
-            <View style={styles.container}>
-                <View style={styles.Headercontainer}>
-                    <TouchableOpacity
-                        onPress={() => {
-                            console.log('hi', this.props.navigation.goBack);
-                            this.props.navigation.goBack();
-                        }}
-                        style={styles.backIcon}
-                    >
-                        <Ionicons name='ios-arrow-back' size={20 * PIXEL_RATE} style={{ marginTop: 2, color: 'black', backgroundColor: 'rgba(0,0,0,0)' }} />
-                    </TouchableOpacity>
-                    <View style={styles.optionContainer}>
+            <ScrollView style={styles.container}>
+                <ImageBackground source={{ uri: "https://img-blog.csdn.net/20180528134720418" }} style={styles.cover}>
+                    <View style={styles.Headercontainer}>
                         <TouchableOpacity
-                            activeOpacity={1}
                             onPress={() => {
-                                this.props.experience.setCurrentTab(1);
+                                console.log('hi', this.props.navigation.goBack);
+                                this.props.navigation.goBack();
                             }}
-                            style={this.props.experience.currentTab === 1 ?
-                            styles.activeTabButton
-                            : styles.inactiveTabButton}
+                            style={styles.backIcon}
                         >
-                            <Text style={{
-                                fontSize: 16
-                            }}>体验</Text>
+                            <Ionicons name='ios-arrow-back' size={20 * PIXEL_RATE} style={{ marginTop: 2, color: 'white', backgroundColor: 'rgba(0,0,0,0)' }} />
                         </TouchableOpacity>
-                        <TouchableOpacity
-                            activeOpacity={1}
+                        <View style={styles.optionContainer}>
+                            <TouchableOpacity
+                                activeOpacity={1}
+                                onPress={() => {
+                                    this.props.experience.setCurrentTab(1);
+                                }}
+                                style={this.props.experience.currentTab === 1 ?
+                                styles.activeTabButton
+                                : styles.inactiveTabButton}
+                            >
+                                <Text style={{
+                                    fontSize: 16,
+                                    color: "white",
+                                }}>体验</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                activeOpacity={1}
+                                onPress={() => {
+                                    this.props.experience.setCurrentTab(2);
+                                }}
+                                style={this.props.experience.currentTab === 2 ?
+                                styles.activeTabButton
+                                : styles.inactiveTabButton}
+                            >
+                                <Text style={{
+                                    fontSize: 16,
+                                    color: "white",
+                                }}>评论</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                    <View style={styles.coverDetail}>
+                        <Text style={styles.experienceTitle}>{this.props.experience.detail.data.experience_title}</Text>
+                        <Text style={styles.description}>{this.props.experience.detail.data.experience_brief_decription}</Text>
+                        <Text style={styles.autor}>by{this.props.experience.detail.data.experience_author}</Text>
+                    </View>
+                </ImageBackground>
+
+                <View style={styles.tagsContainer}>
+                    {this.rendTags()}
+                </View>
+
+                <View style={styles.recommend}>
+                    <Text style={styles.title}>我为什么推荐</Text>
+                    <View style={styles.recomendDetail}>
+                        <Image source={{ uri: 'https://img-blog.csdn.net/20180528164959640'}} style={styles.autorProfile} />
+                        <Text style={[styles.context, {
+                            marginLeft: 15 * PIXEL_RATE,
+                            width: 236 * PIXEL_RATE,}]
+                        }>{this.props.experience.detail.data.recommend_reason}</Text>
+                    </View>
+                </View>
+                <View style={styles.seperator} />
+                
+                <View style={styles.introduction}>
+                    <Text style={styles.title}>体验介绍</Text>
+                    <Image source={{ uri: 'https://img-blog.csdn.net/20180528173335773'}} style={styles.introductionImage}/>
+                    <Text style={[
+                                styles.context, {
+                                marginHorizontal: 44 * PIXEL_RATE,
+                                marginTop: 19 * PIXEL_RATE,
+                        }]} 
+                        numberOfLines={this.props.experience.introductionFold ? 5 : 100}
+                    >{this.props.experience.detail.data.experience_introduction}</Text>
+                    <View style={styles.foldOrExpand}>
+                        <View style={{flex: 1}} />
+                        <TouchableOpacity 
                             onPress={() => {
-                                this.props.experience.setCurrentTab(2);
-                            }}
-                            style={this.props.experience.currentTab === 2 ?
-                            styles.activeTabButton
-                            : styles.inactiveTabButton}
+                                this.props.experience.introductionFold = !this.props.experience.introductionFold
+                            }} 
+                            style={{flexDirection: 'row',}}
                         >
-                            <Text style={{
-                                fontSize: 16
-                            }}>评论</Text>
+                            <Text style={styles.context}
+                            >{this.props.experience.introductionFold ? "展开" : "收起"}</Text>
+                            <Ionicons name={this.props.experience.introductionFold ? 'ios-arrow-down' : 'ios-arrow-up'} style={{margin: 2,}} size={20 * PIXEL_RATE}/>
                         </TouchableOpacity>
                     </View>
                 </View>
-                <Text>{`体验:${this.props.experience.currentExperienceID}`}</Text>
-            </View>
+                <View style={styles.seperator} />
+
+                <View style={styles.importanceInformation}>
+                    <Text style={styles.title}>重点信息</Text>
+                    <Text style={[
+                        styles.context, {
+                            marginHorizontal: 44 * PIXEL_RATE,
+                        }]} 
+                    >{this.props.experience.detail.data.stress_infomation}
+                    </Text>
+                </View>
+
+                <View style={styles.nearbyExperience}>
+                    <View style={styles.nearbyHeader}>
+                        <View style={[styles.seperator, {width: 109 * PIXEL_RATE}]} />
+                        <Text style={{fontSize: 16, marginTop: 30,}}>附近体验</Text>
+                        <View style={[styles.seperator, { width: 109 * PIXEL_RATE }]} />
+                    </View>
+                    <View style={styles.nearbyContext}>
+                        {this.props.experience.detail.data.nearby_experience.map((item, index) => {
+                            return <Item title={item.experience_title}
+                                        description={item.experience_brief_discription} 
+                                        name={item.feature}
+                                        photo='https://img-blog.csdn.net/20180528173335773'
+                                        id={item.experience_id}
+                                        navigation={this.props.navigation}
+                                        experience={this.props.experience} 
+                                        key={index} 
+                                    />
+                        })}
+                    </View>
+                </View>
+
+                <View style={styles.bottomBar}>
+                </View>
+
+  
+            </ScrollView>
         )
     }
 }
@@ -73,8 +168,10 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#f5f5f5',
-        justifyContent: 'center',
-        alignItems: 'center'
+    },
+    cover: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
     },
     Headercontainer: {
         height: 32 * PIXEL_RATE,
@@ -104,7 +201,7 @@ const styles = StyleSheet.create({
     activeTabButton: {
         width: 75,
         height: 36,
-        borderColor: 'black',
+        borderColor: 'white',
         borderRadius: 20,
         borderWidth: 1,
         justifyContent: 'center',
@@ -122,6 +219,102 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         marginLeft: 5,
         marginRight: 5,
+    },
+    coverDetail: {
+        marginLeft: 21 * PIXEL_RATE,
+        marginTop: 448 * PIXEL_RATE_Y,
+    },
+    experienceTitle: {
+        color: 'white',
+        fontSize: 28,
+        marginTop: 10,
+    },
+    description: {
+        color: 'white',
+        fontSize: 14,
+        marginTop: 3,
+    },
+    autor: {
+        color: 'white',
+        fontSize: 14,
+        marginTop: 20,
+    },
+    tagsContainer: {
+        marginTop: 20,
+        marginHorizontal: 16,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    },
+    tags: {
+        margin: 4,
+        fontSize: 12,
+        paddingVertical: 8,
+        paddingHorizontal: 13,
+        borderWidth: StyleSheet.hairlineWidth,
+        borderRadius: 16,
+        borderColor: 'rgba(187, 187, 187, 1)',
+    },
+    title: {
+        marginLeft: 25,
+        fontSize: 18,
+        marginVertical: 20,
+    },
+    recomendDetail: {
+        marginRight: 33 * PIXEL_RATE,
+        marginLeft: 42 * PIXEL_RATE,
+        flexDirection: 'row',
+    },
+    autorProfile: {
+        width: 49 * PIXEL_RATE,
+        height: 49 * PIXEL_RATE,
+        borderRadius: 24.5 * PIXEL_RATE,
+    },
+    context: { 
+        fontSize: 14, 
+        lineHeight: 25,
+    },
+    seperator: {
+        borderWidth: StyleSheet.hairlineWidth,
+        opacity: 0.38,
+        marginLeft: 30,
+        marginRight: 30,
+        marginTop: 30,
+    },
+    introduction: {
+
+    },
+    introductionImage: {
+        width: 375 * PIXEL_RATE,
+        height: 238 * PIXEL_RATE,
+    },
+    foldOrExpand: {
+        flexDirection: 'row',
+        marginRight: 35,
+    },
+    nearbyExperience: {
+        width: Dimensions.get('window').width,
+        backgroundColor: 'rgba(245, 245, 245, 1)',
+        justifyContent: 'center',
+        alignContent: 'center',
+    },
+    nearbyHeader: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
+    nearbyContext: {
+        marginTop: 8 * PIXEL_RATE,
+        marginHorizontal: 11 * PIXEL_RATE,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between'
+    },
+    bottomBar: {
+        width: Dimensions.get('window').width,
+        height: 45 * PIXEL_RATE,
+        backgroundColor: 'grey',
+        position: 'absolute',
+        bottom: 0,
     }
 
 });
--- src/store/experience.js
+++ src/store/experience.js
@@ -14,6 +14,24 @@ class experienceSotre {
     @observable showOriginalSearchBar = true;
     @observable currentExperienceID = '';
     @observable currentTab = 1;
+    @observable introductionFold = true;
+    @observable detail = { "code": 200, "enmsg": "ok", "cnmsg": "成功", 
+                            "data": {   "cover_img": "url", 
+                                        "experience_title": "卡帕多奇亚·星球地貌", 
+                                        "experience_brief_decription": "如果你想去月球，不妨先来这里看看",
+                                         "experience_author": "一粒尘埃", 
+                                         "author_profile_img": "url",
+                                        "tags": ["土耳其", "奇异", "壮观", "浪漫"], 
+                                        "recommend_reason": "在这幽美的山谷两边，到处的是大块的石头，倘若你仔细看在石头，会发现上面都是密密麻麻大小不一的鸽子洞，实际上这是一些特大号的圆锥形岩层。松软的岩石酷似锥形的尖塔，尖塔顶端被大自然赋予了一块更加松软的玄武岩“帽子”。", 
+                                "experience_introduction": "卡帕多奇亚是世界至壮观的“风化区”，触目所及尽是被“吹残”后的天然石雕。卡帕多奇亚奇石林，此地奇特之天然奇景，举世闻名，其大约于三百 万年前，由于火山爆发，熔岩及火山灰覆盖该地，后经长期风化侵蚀，成为现在特殊地形。千姿百态的石头，各种稀奇古怪的 造型，使人感叹是否来到了外星球。美国的科幻大片《星球大战》曾在此取景。由于风景独特，联合国现已将该区列入“世界遗产”的名册内。", 
+                                        "experience_introduction_imgs": ["url"], 
+                                "stress_infomation": "最佳旅行时间：4-5月、9-10月•  穿衣指南：全年注意防晒，除了夏天，其他三季要注意早晚的保暖。•  货币：土耳其通用货币是里拉。常用的纸币面值有5、10、20、50、100和200里拉。门票：成人200里拉，儿童100里拉", 
+                                        "nearby_experience": [{ "experience_id": "000000", "feature": "奇异", "card_img": "url", "experience_title": "卡帕多奇亚：星球地貌", "experience_brief_discription": "如果你想去月球，不妨先来这里看看" }, 
+                                                                { "experience_id": "000001", "feature": "奇异", "card_img": "url", "experience_title": "卡帕多奇亚：星球地貌", "experience_brief_discription": "如果你想去月球，不妨先来这里看看" }, 
+                                                                { "experience_id": "000002", "feature": "奇异", "card_img": "url", "experience_title": "卡帕多奇亚：星球地貌", "experience_brief_discription": "如果你想去月球，不妨先来这里看看" },
+                                                                { "experience_id": "000003", "feature": "奇异", "card_img": "url", "experience_title": "卡帕多奇亚：星球地貌", "experience_brief_discription": "如果你想去月球，不妨先来这里看看" }] 
+                            } 
+    }
 
     @action.bound
     setCurrentTab(id) {
